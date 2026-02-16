a43663ac6a9857970cce79e240207c5029eb43c0
Waqas <90726841+waqas288@users.noreply.github.com>
2026-02-16 13:35:15 +0500

    removed the fixed path
    Updated file paths and comments for clarity.

--- QpWave/qpwave_iran.R
+++ QpWave/qpwave_iran.R
@@ -7,7 +7,7 @@
 library(admixtools)
 
 #Set working directory and paths
-aadr_path <- "C:/Users/WAQAS/Documents/CG/1/22/AA"
+aadr_path <- "path/to/aadr/folder"
 prefix <- file.path(aadr_path, "v62.0_1240k_public")
 
 #Preparing for f2
@@ -326,4 +326,5 @@ qp_iran_1$rankdrop
 
 qp_iran_2 = qpwave(f2_blocks_iran_outgroups_set_2, left=left_iran, right=right_set_2)
 qp_iran_2$rankdrop
+
 
